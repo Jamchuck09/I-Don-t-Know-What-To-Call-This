18b0f3a155cd987b63ff9898bb936de70b3f7032
Jamchuck09 <98853810+Jamchuck09@users.noreply.github.com>
2022-05-12 18:06:57 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+   printf("Here is the source code for this sh*t ");
+   getchar();
+   printf("#include <stdio.h>\n");
+   printf("#include <stdlib.h>\n");
+   printf("int main(){\n");
+   printf("printf(Here is the source code for this sh*t);\n");
+   printf("getchar();\n");
+   printf("return 0;\n");
+   printf("}\n");
+   getchar();
+   return 0;
+}
